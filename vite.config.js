7edc59dd3248c421955a05ea93140eea6bafd686
unnatitessct <unnati@tessact.com>
2022-12-26 09:59:08 +0530

    feat(main): add initial dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  return defineConfig({
+    build: {
+      outDir: 'build',
+    },
+    plugins: [react({ jsxRuntime: 'classic' })], // react() is used to avoid the manual import of React in .jsx and .tsx modules
+    define: {
+      'process.env.NODE_ENV': `"${mode}"`,
+    },
+    server: {
+      port: 3000,
+      open: true,
+    },
+  });
+};
